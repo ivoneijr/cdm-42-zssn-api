0c03970bde22bced60130626458f38e73e984241
IVONEI JUNIOR <ivoneijr@gmail.com>
2017-04-20 16:14:27 -0300

    add more reports.

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -1,8 +1,8 @@
 class API::V1::ReportsController < ApplicationController
 
-  # GET /api/v1/reports?
+  # GET /api/v1/reports
   def index
-    render json: Report.main
+    render json: Report.build
   end
 
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,20 +1,51 @@
 class Report
-  def self.main
-    total = Survivor.count.to_f
 
-    infecteds = Survivor.where(infected: true).count.to_f
-    not_infecteds = Survivor.where(infected: false).count.to_f
+  def self.build
+    infecteds = Survivor.where(infected: true)
+    not_infecteds = Survivor.where(infected: false)
 
     {
-        survivors: total,
+        total_survivors: Survivor.count.to_f,
         infecteds: {
-            total: infecteds,
-            percentage: (infecteds / total * 100).round(2)
+            total: infecteds.count.to_f,
+            percentage: get_percentage(infecteds),
+            survivors: infecteds.as_json(only: [:id, :name])
         },
         not_infecteds: {
-            total: not_infecteds,
-            percentage: (not_infecteds / total * 100).round(2)
+            total: not_infecteds.count.to_f,
+            percentage: get_percentage(not_infecteds),
+            survivors: not_infecteds.as_json(only: [:id, :name])
+        },
+        items_average_by_survivor: {
+          water: average_for(:water),
+          food:  average_for(:food),
+          medication:  average_for(:medication),
+          ammunition:  average_for(:ammunition)
         }
     }
+  end
+
+  def self.get_percentage(collection)
+    (collection.count.to_f / Survivor.count.to_f * 100).round(2)
+  end
+
+  def self.average_for(symbol)
+    map = []
+
+    case symbol
+      when :water
+        map = Inventory.where(item_id: 1).map(&:quantity)
+
+      when :food
+        map = Inventory.where(item_id: 2).map(&:quantity)
+
+      when :medication
+        map = Inventory.where(item_id: 3).map(&:quantity)
+
+      when :ammunition
+        map = Inventory.where(item_id: 4).map(&:quantity)
+    end
+
+    map.empty? ? 0 : map.sum / map.length
   end
 end
